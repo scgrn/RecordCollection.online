6ac17b5ed77da87ab82a0010c220271eb285183c
Andrew Krause <ajkrause@gmail.com>
2023-07-08 23:10:18 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+require('dotenv').config()
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+// http://localhost/
+app.get('/', (request, response) => {
+    // redirect to HOME if user is logged in
+    if (request.session.loggedIn) {
+        response.redirect('/home');
+        return;
+    }
+    
+    // render login template
+    response.render("index");
+});
+
+const userRouter = require('./routes/users.js');
+app.use('/', userRouter);
+
+const panelRouter = require('./routes/panel.js');
+app.use('/', panelRouter);
+
+const collectionRouter = require('./routes/collection.js');
+app.use('/', collectionRouter);
+
+// start server
+const port = 80;
+app.listen(port, () => {
+    console.log(new Date().toISOString() + " | Server running at port " + port);
+});
---
+++ routes/panel.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+// http://localhost/home
+router.get('/home', (request, response) => {
+    if (request.session.loggedIn) {
+        // TODO: WHY DOES IT PRINT THIS TWICE
+        // https://stackoverflow.com/questions/72413113/expressjs-routes-are-invoked-twice-when-running-server-and-browsing-using-browse
+        console.log("HERE");
+        
+        // output username
+        var message;
+        if (request.session.firstLogin) {
+            request.session.firstLogin = false;
+            message = 'Hello, ' + request.session.username + '!';
+        } else {
+            message = 'Welcome back, ' + request.session.username + '!';
+        }
+        
+        // serve file
+        response.render("../views/home", { message: message });
+    } else {
+        // not logged in
+        console.log("NOT LOGGED IN, REDIRECTING HOME.");
+        response.redirect('/');
+    }
+});
+
+router.post('/search', (request, response) => {
+    var Discogs = require('disconnect').Client;
+    var dis = new Discogs('RecordCollection.online/1.0', {
+        consumerKey: process.env.DISCOGS_CONSUMER_KEY,
+        consumerSecret: process.env.DISCOGS_CONSUMER_SECRET
+    });
+
+    var results = [];
+    var promises = [];
+
+    var db = dis.database();
+
+    db.search(request.body.searchTerms, {type: "master"}).then((data) => {
+        for (let result in data.results) {
+            let filename = "./static/img/thumbnails/" + data.results[result].master_id + ".jpg";
+
+            // check if thumbnail has already been downloaded
+            if (!fs.existsSync(filename)) {
+                let thumbnailPromise = db.getImage(data.results[result].thumb);
+                promises.push(thumbnailPromise);
+
+                thumbnailPromise.then((thumbnailData) => {
+                    fs.writeFileSync(filename, thumbnailData, 'binary');
+                });
+            }
+            results.push({
+                thumbnail: "img/thumbnails/" + data.results[result].master_id + ".jpg",
+                title: data.results[result].title,
+                id: data.results[result].master_id
+            });
+        }
+    }).then(() => {
+        //  wait for all thumbnails to download
+        Promise.all(promises).then(() => {
+            var json = JSON.stringify(results);
+            
+            response.send(json);
+            response.end();
+        });
+    }).catch((error) => console.log(error));
+});
+
+module.exports = router;
