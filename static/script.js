5915b54a0205a4c9af4ddac1dc2b5f2857a3a6ff
Andrew Krause <ajkrause@gmail.com>
2023-08-24 21:32:40 -0400

    Fix eyeballs

--- static/script.js
+++ static/script.js
@@ -8,6 +8,7 @@ function initPasswordToggle(inputId, eyeId) {
 
     eye.addEventListener("click", function() {
         this.classList.toggle("fa-eye-slash")
+        this.classList.toggle("fa-eye")
         const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
         passwordInput.setAttribute("type", type)
     });
