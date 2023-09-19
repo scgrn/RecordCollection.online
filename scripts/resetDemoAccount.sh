cd4de4545c64bb65670aebacfa27518f5048e85d
Andrew Krause <ajkrause@gmail.com>
2023-09-18 22:01:34 -0400

    Fix resetDemoAccount script

--- scripts/resetDemoAccount.sh
+++ scripts/resetDemoAccount.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mysql --defaults-file=~/recordcollection.online/utils/resetDemoAccount.cnf -h mysql.recordcollection.online recordcollection_online < ~/recordcollection.online/utils/resetDemoAccount.sql
+mysql --defaults-file=~/recordcollection.online/scripts/resetDemoAccount.cnf -h mysql.recordcollection.online recordcollection_online < ~/recordcollection.online/scripts/resetDemoAccount.sql
 
 
